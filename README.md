bb846a27ebd65f33b6cddc7ae15deae50b8efde9
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-08-21 23:31:50 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ice Cream Mod
+
+This is a MOD for Forge to add some fancy ice cream items.
+
+Based on forge-1.18.2-40.1.73-mdk.
---
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/main/FancyIceCreamMod.java
@@ -0,0 +1,8 @@
+package com.ksoichiro.mcmod.fancyicecream.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FancyIceCreamMod.MOD_ID)
+public class FancyIceCreamMod {
+    public static final String MOD_ID = "fancyicecreammod";
+}
