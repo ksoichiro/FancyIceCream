5214ba3d4af88b8c814f0b382b3ec843ba7d44b6
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-08-21 23:48:49 +0900

    Add Creative Mode tab

--- src/main/java/com/ksoichiro/mcmod/fancyicecream/main/FancyIceCreamMod.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/main/FancyIceCreamMod.java
@@ -5,4 +5,5 @@ import net.minecraftforge.fml.common.Mod;
 @Mod(FancyIceCreamMod.MOD_ID)
 public class FancyIceCreamMod {
     public static final String MOD_ID = "fancyicecreammod";
+    public static final FancyIceCreamModTab FANCY_ICE_CREAM_MOD_TAB = new FancyIceCreamModTab();
 }
---
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/main/FancyIceCreamModTab.java
@@ -0,0 +1,16 @@
+package com.ksoichiro.mcmod.fancyicecream.main;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+public class FancyIceCreamModTab extends CreativeModeTab {
+    public FancyIceCreamModTab() {
+        super("fancyicecreammod_tab");
+    }
+
+    @Override
+    public ItemStack makeIcon() {
+        return new ItemStack(Items.APPLE);
+    }
+}
