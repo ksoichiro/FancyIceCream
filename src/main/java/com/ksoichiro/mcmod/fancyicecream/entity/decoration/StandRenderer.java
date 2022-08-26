0e4708a2b15fa689a00f2fe1250ad3c34885fb01
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-08-27 01:41:56 +0900

    Render item

--- src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/decoration/StandRenderer.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/decoration/StandRenderer.java
@@ -7,24 +7,26 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.Sheets;
 import net.minecraft.client.renderer.block.BlockRenderDispatcher;
+import net.minecraft.client.renderer.block.model.ItemTransforms;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.MapItem;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.saveddata.maps.MapItemSavedData;
 import net.minecraft.world.phys.Vec3;
 
 public class StandRenderer extends EntityRenderer<Stand> {
     public static final ResourceLocation STAND_LOCATION = new ResourceLocation(FancyIceCreamMod.MOD_ID, "block/stand");
     private final Minecraft minecraft = Minecraft.getInstance();
+    private final ItemRenderer itemRenderer;
 
     public StandRenderer(EntityRendererProvider.Context context) {
         super(context);
+        this.itemRenderer = context.getItemRenderer();
     }
 
     @Override
@@ -47,27 +49,21 @@ public class StandRenderer extends EntityRenderer<Stand> {
         poseStack.translate((double)direction.getStepX() * 0.46875D, (double)direction.getStepY() * 0.46875D, (double)direction.getStepZ() * 0.46875D);
         poseStack.mulPose(Vector3f.XP.rotationDegrees(stand.getXRot()));
         poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0F - stand.getYRot()));
-        boolean flag = stand.isInvisible();
         ItemStack itemstack = stand.getItem();
-        if (!flag) {
-            BlockRenderDispatcher blockrenderdispatcher = this.minecraft.getBlockRenderer();
-            ModelManager modelmanager = blockrenderdispatcher.getBlockModelShaper().getModelManager();
-            poseStack.pushPose();
-            poseStack.translate(-0.5D, -0.5D, -0.5D);
-            blockrenderdispatcher.getModelRenderer().renderModel(poseStack.last(), p_115080_.getBuffer(Sheets.solidBlockSheet()), (BlockState)null, modelmanager.getModel(STAND_LOCATION), 1.0F, 1.0F, 1.0F, p_115081_, OverlayTexture.NO_OVERLAY);
-            poseStack.popPose();
-        }
+
+        BlockRenderDispatcher blockrenderdispatcher = this.minecraft.getBlockRenderer();
+        ModelManager modelmanager = blockrenderdispatcher.getBlockModelShaper().getModelManager();
+        poseStack.pushPose();
+        poseStack.translate(-0.5D, -0.5D, -0.5D);
+        blockrenderdispatcher.getModelRenderer().renderModel(poseStack.last(), p_115080_.getBuffer(Sheets.solidBlockSheet()), (BlockState)null, modelmanager.getModel(STAND_LOCATION), 1.0F, 1.0F, 1.0F, p_115081_, OverlayTexture.NO_OVERLAY);
+        poseStack.popPose();
 
         if (!itemstack.isEmpty()) {
-            MapItemSavedData mapitemsaveddata = MapItem.getSavedData(itemstack, stand.level);
-            if (flag) {
-                poseStack.translate(0.0D, 0.0D, 0.5D);
-            } else {
-                poseStack.translate(0.0D, 0.0D, 0.4375D);
-            }
-
-            int j = mapitemsaveddata != null ? stand.getRotation() % 4 * 2 : stand.getRotation();
-            poseStack.mulPose(Vector3f.ZP.rotationDegrees((float)j * 360.0F / 8.0F));
+            poseStack.translate(0.0D, 0.0D, 0.1D);
+            poseStack.mulPose(Vector3f.XP.rotationDegrees(-70.0F));
+            poseStack.mulPose(Vector3f.ZP.rotationDegrees(-45.0F));
+            poseStack.scale(0.5F, 0.5F, 0.5F);
+            this.itemRenderer.renderStatic(itemstack, ItemTransforms.TransformType.FIXED, p_115081_, OverlayTexture.NO_OVERLAY, poseStack, p_115080_, stand.getId());
         }
 
         poseStack.popPose();
