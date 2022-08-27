624e97e35b996173a15e8e6026341bc4c526fac3
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-08-28 07:49:37 +0900

    Get stand item when broken

--- src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/decoration/Stand.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/decoration/Stand.java
@@ -1,10 +1,12 @@
 package com.ksoichiro.mcmod.fancyicecream.entity.decoration;
 
 import com.ksoichiro.mcmod.fancyicecream.entity.FancyIceCreamModEntityType;
+import com.ksoichiro.mcmod.fancyicecream.registry.FancyIceCreamModItems;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.decoration.ItemFrame;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
 public class Stand extends ItemFrame {
@@ -14,5 +16,10 @@ public class Stand extends ItemFrame {
 
     public Stand(Level level, BlockPos pos, Direction direction) {
         super(FancyIceCreamModEntityType.STAND, level, pos, direction);
+    }
+
+    @Override
+    public ItemStack getFrameItemStack() {
+        return new ItemStack(FancyIceCreamModItems.STAND);
     }
 }
