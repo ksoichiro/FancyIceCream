df3b27a00a77e5c8a9f6d1190ad9be649a731a91
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-09-13 23:16:37 +0900

    Apply intermediate layer to items

--- src/main/java/com/ksoichiro/mcmod/fancyicecream/item/ChocolateIceCream.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/item/ChocolateIceCream.java
@@ -1,13 +1,11 @@
 package com.ksoichiro.mcmod.fancyicecream.item;
 
-import com.ksoichiro.mcmod.fancyicecream.main.FancyIceCreamMod;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 
 public class ChocolateIceCream extends Item {
     public ChocolateIceCream() {
-        super(new Properties().tab(FancyIceCreamMod.FANCY_ICE_CREAM_MOD_TAB)
-                .food(new FoodProperties.Builder()
+        super(new ItemProperties()
+                .food(new FoodBuilder()
                         .nutrition(4)
                         .saturationMod(0.3F)
                         .build()));
--- src/main/java/com/ksoichiro/mcmod/fancyicecream/item/GoldenAppleIceCream.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/item/GoldenAppleIceCream.java
@@ -1,19 +1,15 @@
 package com.ksoichiro.mcmod.fancyicecream.item;
 
-import com.ksoichiro.mcmod.fancyicecream.main.FancyIceCreamMod;
-import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 
 public class GoldenAppleIceCream extends Item {
     public GoldenAppleIceCream() {
-        super(new Properties().tab(FancyIceCreamMod.FANCY_ICE_CREAM_MOD_TAB)
-                .food(new FoodProperties.Builder()
+        super(new ItemProperties()
+                .food(new FoodBuilder()
                         .nutrition(4)
                         .saturationMod(1.2F)
-                        .effect(() -> new MobEffectInstance(MobEffects.REGENERATION, 100, 1), 1.0F)
-                        .effect(() -> new MobEffectInstance(MobEffects.ABSORPTION, 2400, 0), 1.0F)
+                        .effect(Effects.REGENERATION, 100, 1, 1.0F)
+                        .effect(Effects.ABSORPTION, 2400, 0, 1.0F)
                         .alwaysEat()
                         .build()));
     }
