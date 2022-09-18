4dea1b2d3d0eb4c2b5cef2c42d56799ad8210c3c
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-09-18 22:52:31 +0900

    Add ice cream block model

--- src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/decoration/IceCreamStandRenderer.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/decoration/IceCreamStandRenderer.java
@@ -12,12 +12,17 @@ import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
+import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.client.resources.model.ModelManager;
 import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.List;
 
 public class IceCreamStandRenderer<T extends IceCreamStand> extends EntityRenderer<T> {
     public static final ResourceLocation STAND_LOCATION = new ResourceLocation(FancyIceCreamMod.MOD_ID, "block/ice_cream_stand");
@@ -65,14 +70,47 @@ public class IceCreamStandRenderer<T extends IceCreamStand> extends EntityRender
     }
 
     protected void renderStandItems(T iceCreamStand, PoseStack poseStack, MultiBufferSource bufferIn, int packedLightIn) {
-        ItemStack itemstack = iceCreamStand.getItem(0);
-        if (!itemstack.isEmpty()) {
-            poseStack.scale(0.5F, 0.5F, 0.5F);
-            poseStack.translate(0.0D, 0.1D, -0.0D);
-            poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0F));
-            poseStack.mulPose(Vector3f.XP.rotationDegrees(20.0F));
-            poseStack.mulPose(Vector3f.ZP.rotationDegrees(-45.0F));
-            this.itemRenderer.renderStatic(itemstack, ItemTransforms.TransformType.FIXED, packedLightIn, OverlayTexture.NO_OVERLAY, poseStack, bufferIn, iceCreamStand.getId());
+        List<ItemStack> itemstacks = iceCreamStand.getItems();
+        double[][] translations = getTranslations();
+        for (int i = 0; i < itemstacks.size(); i++) {
+            ItemStack itemstack = itemstacks.get(i);
+            if (!itemstack.isEmpty()) {
+                poseStack.pushPose();
+                BlockRenderDispatcher blockrenderdispatcher = this.minecraft.getBlockRenderer();
+                ModelManager modelmanager = blockrenderdispatcher.getBlockModelShaper().getModelManager();
+                BakedModel model = getBlockModel(modelmanager, itemstack);
+                if (model.equals(modelmanager.getMissingModel())) {
+                    poseStack.translate(translations[i][0] / 16.0D, translations[i][1] / 16.0D, translations[i][2] / 16.0D);
+                    poseStack.scale(0.5F, 0.5F, 0.5F);
+                    poseStack.mulPose(Vector3f.YP.rotationDegrees(180.0F));
+                    poseStack.mulPose(Vector3f.XP.rotationDegrees(20.0F));
+                    poseStack.mulPose(Vector3f.ZP.rotationDegrees(-45.0F));
+                    this.itemRenderer.renderStatic(itemstack, ItemTransforms.TransformType.FIXED, packedLightIn, OverlayTexture.NO_OVERLAY, poseStack, bufferIn, iceCreamStand.getId());
+                } else {
+                    poseStack.translate(translations[i][0] / 16.0D, translations[i][1] / 16.0D, translations[i][2] / 16.0D);
+                    poseStack.scale(0.8F, 0.8F, 0.8F);
+                    poseStack.translate(-0.5D, -0.5D, -0.5D);
+                    blockrenderdispatcher.getModelRenderer().renderModel(poseStack.last(), bufferIn.getBuffer(Sheets.solidBlockSheet()), (BlockState)null, model, 1.0F, 1.0F, 1.0F, packedLightIn, OverlayTexture.NO_OVERLAY);
+                }
+                poseStack.popPose();
+            }
         }
+    }
+
+    protected BakedModel getBlockModel(ModelManager modelmanager, ItemStack itemStack) {
+        Item item = itemStack.getItem();
+        String namespace = "minecraft";
+        ResourceLocation itemResource = ForgeRegistries.ITEMS.getKey(item);
+        if (itemResource != null) {
+            namespace = itemResource.getNamespace();
+        }
+        ResourceLocation modelResource = new ResourceLocation(namespace, "block/" + item);
+        return modelmanager.getModel(modelResource);
+    }
+
+    protected double[][] getTranslations() {
+        return new double[][]{
+                {0.0D, 0.1D, 0.0D},
+        };
     }
 }
