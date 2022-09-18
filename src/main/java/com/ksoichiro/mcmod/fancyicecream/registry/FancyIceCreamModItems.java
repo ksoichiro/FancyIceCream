43dceede27f3ff17d576617f00fd820b0506f1a2
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-09-18 21:28:51 +0900

    Add apple ice cream model

--- src/main/java/com/ksoichiro/mcmod/fancyicecream/registry/FancyIceCreamModItems.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/registry/FancyIceCreamModItems.java
@@ -40,6 +40,7 @@ public class FancyIceCreamModItems {
         @SubscribeEvent
         public static void registerModels(final ModelEvent.RegisterAdditional event) {
             event.register(new ResourceLocation(FancyIceCreamMod.MOD_ID, "block/vanilla_ice_cream"));
+            event.register(new ResourceLocation(FancyIceCreamMod.MOD_ID, "block/apple_ice_cream"));
             event.register(new ResourceLocation(FancyIceCreamMod.MOD_ID, "block/choco_chip_ice_cream"));
             event.register(new ResourceLocation(FancyIceCreamMod.MOD_ID, "block/chocolate_ice_cream"));
         }
