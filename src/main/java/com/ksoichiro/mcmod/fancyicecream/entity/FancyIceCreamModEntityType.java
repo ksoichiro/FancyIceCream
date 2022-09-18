1c7d066b71c6762a29beca29e38f50907cc414e5
Soichiro Kashima <soichiro.kashima@gmail.com>
2022-09-18 23:39:09 +0900

    Revert for 1.17.1

--- src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/FancyIceCreamModEntityType.java
+++ src/main/java/com/ksoichiro/mcmod/fancyicecream/entity/FancyIceCreamModEntityType.java
@@ -9,13 +9,13 @@ import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ForgeModelBakery;
+import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmllegacy.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
 
 public class FancyIceCreamModEntityType {
     private static final DeferredRegister<EntityType<?>> ENTITY_TYPE = DeferredRegister.create(ForgeRegistries.ENTITIES, FancyIceCreamMod.MOD_ID);
@@ -44,8 +44,8 @@ public class FancyIceCreamModEntityType {
     public static class Registerer {
         @SubscribeEvent
         public static void registerModel(final ModelRegistryEvent event) {
-            ForgeModelBakery.addSpecialModel(IceCreamStandRenderer.STAND_LOCATION);
-            ForgeModelBakery.addSpecialModel(TripleIceCreamStandRenderer.STAND_LOCATION);
+            ModelLoader.addSpecialModel(IceCreamStandRenderer.STAND_LOCATION);
+            ModelLoader.addSpecialModel(TripleIceCreamStandRenderer.STAND_LOCATION);
         }
         @SubscribeEvent
         public static void registerRenderer(final EntityRenderersEvent.RegisterRenderers event) {
